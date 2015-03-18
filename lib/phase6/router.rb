4bdb7fbbbf50197a68f91eafcbb0013a8cc0db5e
Dan Dzoan <dandzoan@gmail.com>
2015-03-18 14:04:33 -0700

    finish phase 6, integration tests (phase 7) pass

--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -11,7 +11,7 @@ module Phase6
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
-      req.request_method.to_sym == @http_method
+      req.request_method.downcase.to_sym == @http_method && !!pattern.match(req.path)
     end
 
     # use pattern to pull out route params (save for later?)
@@ -23,8 +23,8 @@ module Phase6
       param_keys.each do |key|
         route_params[key] = match_object[key]
       end
-      
-      controller_class.new(req,res).invoke_action(action_name)
+
+      controller_class.new(req,res,route_params).invoke_action(action_name)
     end
   end
 
@@ -36,25 +36,40 @@ module Phase6
     end
 
     # simply adds a new route to the list of routes
-    def add_route(pattern, method, controller_class, action_name)
+    def add_route(pattern, http_method, controller_class, action_name)
+      @routes << Route.new(pattern, http_method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
     # for syntactic sugar :)
     def draw(&proc)
+      self.instance_eval(&proc)
     end
 
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) do |pattern, controller_class, action_name|
+        add_route(pattern, http_method, controller_class, action_name)
+      end
     end
 
     # should return the route that matches this request
     def match(req)
+      @routes.each do |route|
+        return route if route.matches?(req)
+      end
+      nil
     end
 
     # either throw 404 or call run on a matched route
     def run(req, res)
+      route = match(req)
+      if route
+        route.run(req,res)
+      else
+        res.status = 404
+      end
     end
   end
 end
